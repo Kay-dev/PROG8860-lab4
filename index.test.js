91b29b48f89091dc7d50cad90ef54dba1e6e35ba
zhengwei <a932327192@gmail.com>
2025-03-12 00:46:22 -0400

    update test file

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./server');
+const app = require('./index');
 
 describe('API Endpoints', () => {
     test('GET /health should return status OK', async () => {
